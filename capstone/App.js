511f321e6b5d498f07c4a1ce69a2e49a2dd26b3f
Enceladus-X <xogh7319@daum.net>
2024-04-05 14:19:50 +0900

    네비게이션 적용

--- capstone/App.js
+++ capstone/App.js
@@ -1,21 +1,12 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import AppNavigator from './Navigation/Navigation.jsx';
+import { NavigationContainer } from '@react-navigation/native';
 
-// Import pages
-import  LoginPage  from "./pages/LoginPage";
-import  LoadingPage  from "./pages/LoadingPage";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LoadingPage" component={LoadingPage} />
-        <Stack.Screen name="LoginPage" component={LoginPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <AppNavigator />
   );
 }
--- capstone/Navigation/Navigation.jsx
+++ capstone/Navigation/Navigation.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Import pages
-
-import LoginPage from '../pages/LoginPage';
-import LoadingPage from '../pages/LoadingPage';
-import HomePage from '../pages/homepage';
-import ProfileInputPage from '../pages/ProfileInputPage'
-import SettingPage from '../pages/SettingPage';
-
+import LoginPage from "../pages/LoginPage";
+import LoadingPage from "../pages/LoadingPage";
+import ProfileInputPage from "../pages/ProfileInputPage";
+import SettingPage from "../pages/SettingPage";
+import HomePage from "../pages/HomePage";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="LoginPage" component={LoginPage} />
-                <Stack.Screen name="LoadingPage" component={LoadingPage} />
-                <Stack.Screen name="HomePage" component={HomePage} />
-                <Stack.Screen name="ProfileInputPage" component={ProfileInputPage} />
-                <Stack.Screen name="SettingPage" component={SettingPage} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LoadingPage" component={LoadingPage} />
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="ProfileInputPage" component={ProfileInputPage} />
+        <Stack.Screen name="SettingPage" component={SettingPage} />
+        <Stack.Screen name="HomePage" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default AppNavigator;
