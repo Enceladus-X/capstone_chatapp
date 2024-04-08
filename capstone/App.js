8ec9022e49b2632a7ca9372360f2dda5bff2bd54
Enceladus-X <106950964+Enceladus-X@users.noreply.github.com>
2024-04-08 19:07:44 +0900

    네비게이션 기본헤더 제거

--- capstone/App.js
+++ capstone/App.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import AppNavigator from './Navigation/Navigation.jsx';
-import { NavigationContainer } from '@react-navigation/native';
-
+import React from "react";
+import AppNavigator from "./Navigation/Navigation.jsx";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-
-  
-  return (
-      <AppNavigator />
-  );
+  return <AppNavigator />;
 }
