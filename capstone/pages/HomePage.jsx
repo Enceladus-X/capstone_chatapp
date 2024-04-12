4ffa720ea6645c0c2f092469aba3e4d99743f9a4
Enceladus-X <xogh7319@daum.net>
2024-04-12 10:20:31 +0900

    drawer 추가
    이걸로 SettingPage로 이동할 수 있음

--- capstone/pages/HomePage.jsx
+++ capstone/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import "react-native-gesture-handler";
 import React, { useEffect } from "react";
 import { View } from "react-native";
 import ButtonCustom from "../components/ButtonCustom";
 import Header from "../components/header";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { SettingPage } from "./SettingPage"
 
 function HomePage({ navigation }) {
   useEffect(() => {}, []);
@@ -14,9 +18,18 @@ function HomePage({ navigation }) {
     navigation.navigate("SettingPage");
   };
 
+  const Drawer = createDrawerNavigator();
+
+  const HomeDrawer = () => {
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="SettingPage" component={SettingPage} />
+      </Drawer.Navigator>
+    </NavigationContainer>;
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      <Header text={"Home Page"} />
 
       <ButtonCustom
         onpress={() => {
@@ -30,6 +43,7 @@ function HomePage({ navigation }) {
         }}
         title="go to Setting Page"
       />
+      <HomeDrawer />
     </View>
   );
 }
