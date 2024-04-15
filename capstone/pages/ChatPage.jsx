df73f9c8bd881219cdbcba1699ad3cb6c08f079d
Enceladus-X <106950964+Enceladus-X@users.noreply.github.com>
2024-04-15 17:50:00 +0900

    ChatPage 크기조절
    화면만 채우도록

--- capstone/pages/ChatPage.jsx
+++ capstone/pages/ChatPage.jsx
@@ -5,10 +5,13 @@ import "sendbird-uikit/dist/index.css";
 const APP_ID = "AD3842D8-FC26-42B4-BA66-54098A1BA28D";
 const USER_ID = "xogh7319";
 
-
 function Chatpage() {
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      height: '100%',
+      width: '100%',
+    }}>
       <SendbirdApp
         appId={APP_ID} 
         userId={USER_ID} 
