94c74818859a2bcfb6847d7fa4b29c87970c6f4a
Enceladus-X <106950964+Enceladus-X@users.noreply.github.com>
2024-04-15 17:50:07 +0900

    Update HomePage.jsx

--- capstone/pages/HomePage.jsx
+++ capstone/pages/HomePage.jsx
@@ -5,10 +5,14 @@ import ButtonCustom from "../components/ButtonCustom";
 import Header from "../components/header";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { SettingPage } from "./SettingPage"
+import { SettingPage } from "./SettingPage";
+import AppbarCustom from "../components/AppbarCustom";
+import { Box, List } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function HomePage({ navigation }) {
   useEffect(() => {}, []);
+
 
   const gotoChatPage = () => {
     navigation.navigate("ChatPage");
@@ -18,18 +22,11 @@ function HomePage({ navigation }) {
     navigation.navigate("SettingPage");
   };
 
-  const Drawer = createDrawerNavigator();
 
-  const HomeDrawer = () => {
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="SettingPage" component={SettingPage} />
-      </Drawer.Navigator>
-    </NavigationContainer>;
-  };
 
   return (
     <View style={{ flex: 1 }}>
+      <AppbarCustom title="Welcome" onPress={() => gotoSettingPage()} />
 
       <ButtonCustom
         onpress={() => {
@@ -43,7 +40,6 @@ function HomePage({ navigation }) {
         }}
         title="go to Setting Page"
       />
-      <HomeDrawer />
     </View>
   );
 }
