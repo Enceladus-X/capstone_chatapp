12cd934f118801d86bf2187be1f44c91c9820bdf
GemstoneK <serve3690@gmail.com>
2024-04-15 17:39:53 +0900

    LoginPage, ProfileInputPage 구현
    expo 로 개발함
    button cotainer 쓰면 위에 뜨길래
    button container 안쓰고
    chat GPT의 도움을 받아 직접 만듦
    
    로딩 페이지는 아직 미구현
    로그인 버튼 누르면 프로필 입력 페이지로 넘어가고
    continue 버튼 누르면 홈페이지로 이동함

--- capstone/pages/ProfileInputPage.jsx
+++ capstone/pages/ProfileInputPage.jsx
@@ -1,24 +1,72 @@
-import React, { useEffect } from "react";
-import { View } from "react-native";
-import ButtonCustom from "../components/ButtonCustom";
-import Header from "../components/header";
+import React, { useState } from 'react'
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
 
 function ProfileInputPage({ navigation }) {
-  useEffect(() => {}, []);
+  const [nickName, setNickName] = useState('')
+  const [gender, setGender] = useState('')
+  const [age, setAge] = useState('')
 
   const gotoHomePage = () => {
-    navigation.navigate("HomePage");
-  };
+    navigation.navigate('HomePage')
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <ButtonCustom
-        onpress={() => {
-          gotoHomePage();
-        }}
-        title="go to Home Page"
-      />
+    <View style={styles.container}>
+      <Text>환영합니다!!</Text>
+      <View style={styles.form}>
+        <Text>프로필 입력</Text>
+        <Text>닉네임 입력</Text>
+        <TextInput
+          style={styles.input}
+          value={nickName}
+          onChangeText={(text) => setNickName(text)}
+          placeholder="닉네임을 입력하세요"
+        />
+        <Text>성별 입력</Text>
+        <Picker
+          selectedValue={gender}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
+        >
+          <Picker.Item label="성별을 선택하세요" value="" />
+          <Picker.Item label="남자" value="male" />
+          <Picker.Item label="여자" value="female" />
+        </Picker>
+        <Text>나이 입력</Text>
+        <Picker
+          selectedValue={age}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex) => setAge(itemValue)}
+        >
+          <Picker.Item label="나이를 선택하세요" value="" />
+          {[...Array(101).keys()].map((num) => (
+            <Picker.Item label={String(num)} value={String(num)} key={num} />
+          ))}
+        </Picker>
+
+        <Button title="Continue" onPress={gotoHomePage} />
+      </View>
     </View>
-  );
+  )
 }
 
-export default ProfileInputPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    width: '80%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+})
+
+export default ProfileInputPage
