0a70ab0903eb8d62f2b92b6a8ae8dfd810c074a7
Enceladus-X <106950964+Enceladus-X@users.noreply.github.com>
2024-04-21 22:18:00 +0900

    사용하지 않는 코드 삭제

--- capstone/pages/HomePage.jsx
+++ capstone/pages/HomePage.jsx
@@ -2,13 +2,8 @@ import 'react-native-gesture-handler'
 import React, { useEffect } from 'react'
 import { View } from 'react-native'
 import ButtonCustom from '../components/ButtonCustom'
-// import Header from "../components/header";
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { NavigationContainer } from '@react-navigation/native'
-import { SettingPage } from './SettingPage'
 import AppbarCustom from '../components/AppbarCustom'
-import { Box, List } from '@mui/material'
-import SettingsIcon from '@mui/icons-material/Settings'
+
 
 function HomePage({ navigation }) {
   useEffect(() => {}, [])
