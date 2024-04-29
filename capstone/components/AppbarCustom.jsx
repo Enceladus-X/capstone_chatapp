bac7f384849d5003f0cf8c15454d4bfa1def204f
Enceladus-X <106950964+Enceladus-X@users.noreply.github.com>
2024-04-29 13:01:13 +0900

    appbar 수정
    nativebase로

--- capstone/components/AppbarCustom.jsx
+++ capstone/components/AppbarCustom.jsx
@@ -1,31 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
 
-export default function ButtonAppBar( {onPress = "", title}) {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick = {onPress}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {title}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+
+function AppBar() {
+  return <>
+      <StatusBar bg="#3700B3" barStyle="light-content" />
+      <Box safeAreaTop bg="violet.600" />
+      <HStack bg="violet.800" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
+        <HStack alignItems="center">
+          <IconButton icon={<Icon size="sm" as={MaterialIcons} name="menu" color="white" />} />
+          <Text color="white" fontSize="20" fontWeight="bold">
+            Home
+          </Text>
+        </HStack>
+        <HStack>
+          <IconButton icon={<Icon as={MaterialIcons} name="favorite" size="sm" color="white" />} />
+          <IconButton icon={<Icon as={MaterialIcons} name="search" size="sm" color="white" />} />
+          <IconButton icon={<Icon as={MaterialIcons} name="more-vert" size="sm" color="white" />} />
+        </HStack>
+      </HStack>
+    </>;
+}
+
+function Example() {
+  return <Center>
+      <AppBar />
+    </Center>;
 }
--- capstone/pages/ChatPage.jsx
+++ capstone/pages/ChatPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { App as SendbirdApp } from "sendbird-uikit";
-import "sendbird-uikit/dist/index.css";
+
 
 const APP_ID = "AD3842D8-FC26-42B4-BA66-54098A1BA28D";
 const USER_ID = "xogh7319";
@@ -12,10 +11,7 @@ function Chatpage() {
       height: '100%',
       width: '100%',
     }}>
-      <SendbirdApp
-        appId={APP_ID} 
-        userId={USER_ID} 
-      />
+      
     </div>
   );
 }
