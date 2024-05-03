a33c162d85572b1562d4f4baa2e6c69097529152
Enceladus-X <xogh7319@daum.net>
2024-05-03 14:19:34 +0900

    LoadingPage 주석 추가

--- capstone/pages/LoadingPage.jsx
+++ capstone/pages/LoadingPage.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
 
+// LoadingPage 컴포넌트는 앱의 로딩 화면을 담당하며, 로고 이미지를 통해 홈 페이지로 이동할 수 있음.
 function LoadingPage({ navigation }) {
-  useEffect(() => {}, []);
+  // 홈 페이지로 직접 이동하는 함수
+  const gotoHomePage = () => navigation.navigate("Home");
 
-  const gotoHomePage = () => {
-    navigation.navigate("Home");
-  };
   return (
     <View style={styles.container}>
-      {/* 이미지 추가 */}
-      <TouchableOpacity onPress={()=>{gotoHomePage();}}>
+      {/* 로고 이미지를 터치하면 홈 페이지로 이동 */}
+      <TouchableOpacity onPress={gotoHomePage}>
         <Image
-          source={require("../assets/images/logoText.png")} // 이미지 파일의 경로를 지정합니다.
-          style={styles.image}
+          source={require("../assets/images/logoText.png")} // 이미지 파일 경로
+          style={styles.image} // 이미지 스타일 적용
         />
       </TouchableOpacity>
     </View>
@@ -22,16 +21,15 @@ function LoadingPage({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    flex: 1, // View를 화면 전체에 표시
+    justifyContent: "center", // 자식 요소를 수직 중앙에 배치
+    alignItems: "center", // 자식 요소를 수평 중앙에 배치
   },
   image: {
-    width: 200,
-    height: 200,
-    resizeMode: "contain", // 이미지를 화면에 맞게 조정합니다.
-    marginBottom: 20, // 이미지와 버튼 사이의 간격 조정
-    marginLeft: 28,
+    width: 200, // 이미지의 너비
+    height: 200, // 이미지의 높이
+    resizeMode: "contain", // 이미지의 비율을 유지하며 컨테이너에 맞게 조정
+    marginBottom: 20, // 이미지 아래에 여백 추가
   },
 });
 
