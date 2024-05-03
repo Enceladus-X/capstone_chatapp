72cff566db795e55794a923c1c4455b756be3b2f
Enceladus-X <xogh7319@daum.net>
2024-05-03 14:27:03 +0900

    Router 코드 최적화

--- capstone/Routes/Router.jsx
+++ capstone/Routes/Router.jsx
@@ -1,42 +1,43 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { MaterialIcons } from "@expo/vector-icons";
 
-// Import pages
+// Import page components
 import LoginPage from "../pages/LoginPage";
 import LoadingPage from "../pages/LoadingPage";
 import SettingPage from "../pages/SettingPage";
 import HomePage from "../pages/HomePage";
-import ChatPage from "../pages/Chatpage";
-import { NavigationContainer } from "@react-navigation/native";
+import ChatPage from "../pages/ChatPage";
 
 const Stack = createNativeStackNavigator();
+
+// Define default screen options
+const defaultScreenOptions = {
+  headerStyle: {
+    height: 45,
+  },
+};
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            height: 45,
-          },
-        }}
-      >
+      <Stack.Navigator screenOptions={defaultScreenOptions}>
+        {/* Screen configurations */}
         <Stack.Screen
           name="Loading"
           component={LoadingPage}
-          options={{ headerShown: false }}
+          options={{ headerShown: false }} // No header
         />
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen
           name="Setting"
           component={SettingPage}
-          options={{ headerShown: false }}
+          options={{ headerShown: false }} // No header
         />
         <Stack.Screen
           name="Home"
           component={HomePage}
-          options={{ headerShown: false }}
+          options={{ headerShown: false }} // No header
         />
         <Stack.Screen name="Chat" component={ChatPage} />
       </Stack.Navigator>
